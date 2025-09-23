920da75c4e894437219a9fe894594fe80d2f7da5
VJ13SS <viswajith5749@gmail.com>
2025-09-23 22:02:08 +0530

    addded the prediction function

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-
+from routes import main
 
 app = Flask(__name__)
 CORS(app)
 
+
+app.register_blueprint(main)
+
 @app.route('/')
 def home():
+    
     return 'Hello World'
 
 @app.route('/predict')
---
+++ routes.py
@@ -0,0 +1,31 @@
+from flask import Blueprint,jsonify
+#from tensorflow.keras.models import load_model
+#from tensorflow.keras.utilsimport load_img,img_to_array
+import numpy as np
+
+main = Blueprint('main',__name__)
+
+def predict_image(img_path,model,class_indices):
+    img = load_img(img_path,target_size=(224,224))
+    img_array = img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array,axis=0)
+
+    predictions = model.predict(img_array)
+    predicted_index = np.argmax(predictions,axis=1)[0]
+
+    #Map class to label
+    labels = dict((v,k) for k,v in class_indices.items())
+    return labels[predicted_index]
+
+@main.route('/predict')
+def perdict():
+
+    try:
+        img_path = ""
+        class_indices = {}
+        model = load_model("skin_disease_model.keras")
+        
+        prediction = predict_image(img_path,model,class_indices)
+        return jsonify({'res':prediction}),200
+    except:
+        return jsonify({'error':'Some Error Occured'}),404
