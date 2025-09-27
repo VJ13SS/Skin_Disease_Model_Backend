de238d1ec0458c698403988346862c3102a5b42e
VJ13SS <viswajith5749@gmail.com>
2025-09-27 16:12:39 +0530

    updated the skin disease detection model and the predict route

--- routes.py
+++ routes.py
@@ -1,54 +1,29 @@
 from flask import Blueprint,jsonify,request
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import cv2
-from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.utils import load_img,img_to_array
 import numpy as np
+from io import BytesIO
 
 main = Blueprint('main',__name__)
-
-def predict_image(img_path,model,class_indices):
-    img = load_img(img_path,target_size=(224,224))
-    img_array = img_to_array(img) / 255.0
-    img_array = np.expand_dims(img_array,axis=0)
-
-    predictions = model.predict(img_array)
-    predicted_index = np.argmax(predictions,axis=1)[0]
-
-    #Map class to label
-    labels = dict((v,k) for k,v in class_indices.items())
-    return labels[predicted_index]
 
 @main.route('/predict',methods = ['POST'])
 def perdict():
 
     try:
         data = request.files['file']
-        img_path = data
-        class_indices = {'Actinic keratosis': 0,
- 'Atopic Dermatitis': 1,
- 'Benign keratosis': 2,
- 'Dermatofibroma': 3,
- 'Melanocytic nevus': 4,
- 'Melanoma': 5,
- 'Squamous cell carcinoma': 6,
- 'Tinea Ringworm Candidiasis': 7,
- 'Vascular lesion': 8}
-        model = load_model("skin_disease_model.keras",compile=False)
-
-        file_bytes = np.frombuffer(data.read(),np.uint8)
-        img = cv2.imdecode(file_bytes,cv2.IMREAD_COLOR)
-
-        img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-
-        plt.imshow(img_rgb)
-        plt.savefig('output.png')
+        class_names = ['akiec','bcc','bkl','df','mel','nv','vasc']
         
-        prediction = predict_image(img_path,model,class_indices)
-        print(prediction)
+        model = load_model("models/skin_cancer_model.h5",compile=False)
+        img_path = BytesIO(data.read())
+        img = image.load_img(img_path,target_size=(32,32))
+        img_array = image.img_to_array(img)/255.0
+        img_array = np.expand_dims(img_array,axis=0)
+        
+        predict = model.predict(img_array)
+        res = class_names[np.argmax(predict)]
+
+        print('helo',res)
         return jsonify({'res':'result'}),200
     except Exception as e:
         print(e)
