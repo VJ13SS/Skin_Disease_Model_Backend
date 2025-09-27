f047dc73f07b739879c5795803c637e8f4ab7957
Viswajith S <135973734+VJ13SS@users.noreply.github.com>
2025-09-27 16:10:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,14 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.utils import load_img,img_to_array
 import numpy as np
 
+Melanocytic nevi (nv)
+Melanoma (mel)
+Benign keratosis-like lesions (bkl)
+Basal cell carcinoma (bcc)
+Actinic keratoses (akiec)
+Vascular lesions (vas)
+Dermatofibroma (df)
+
 model = load_model("models/skin_cancer_model.h5",compile=False)
 img_path = 'ISIC_0000166.jpg'
 img = image.load_img(img_path,target_size=(32,32))
